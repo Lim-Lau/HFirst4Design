23d4e93734b54e1ecc4cf7de85bf215c4c597dee
LiuCan <liucan@chinarecrm.com.cn>
2020-12-02 14:18:51 +0800

    :art: swagger-ui pom

--- src/main/java/com/headfirst/design/controller/UserController.java
+++ src/main/java/com/headfirst/design/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.headfirst.design.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,10 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @description UserController
  */
 @RestController
-@Api(tags = {"用户管理"}, value = "用户管理")
+@Api(tags = "首页模块")
 public class UserController {
-    @GetMapping("/list")
-    @ApiOperation(value = "用户列表")
-    public void list() {
+
+    @ApiImplicitParam(name = "name",value = "姓名",required = true)
+    @ApiOperation(value = "向客人问好")
+    @GetMapping("/hi")
+    public ResponseEntity<String> sayHi(@RequestParam(value = "name")String name){
+        return ResponseEntity.ok("Hi:"+name);
     }
 }
